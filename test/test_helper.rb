c5fb8085f310d392421d2e2c40aa4f708e778171
Gabriel Arjones <gabriel.arjones@tidewise.io>
2019-10-06 22:06:06 -0300

    add buildconf manager tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,45 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'autoproj/daemon'
 require 'autoproj/test'
-
 require 'minitest/autorun'
 require 'minitest/spec'
+
+require 'rubygems/package'
+
+def create_pull_request(options = {})
+    Autoproj::Daemon::Github::PullRequest.new(
+        state: options[:state],
+        number: options[:number],
+        title: options[:title],
+        base: {
+            ref: options[:base_branch],
+            sha: options[:base_sha],
+            user: {
+                login: options[:base_owner]
+            },
+            repo: {
+                name: options[:base_name]
+            }
+        },
+        head: {
+            ref: options[:head_branch],
+            sha: options[:head_sha],
+            user: {
+                login: options[:head_owner]
+            },
+            repo: {
+                name: options[:head_name]
+            }
+        }
+    )
+end
+
+def create_branch(owner, name, options = {})
+    Autoproj::Daemon::Github::Branch.new(
+        owner, name,
+        name: options[:branch_name],
+        commit: {
+            sha: options[:sha]
+        }
+    )
+end
