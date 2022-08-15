cbd704b9d97483d35692ff9e70c861a05ac940cf
Sylvain <sylvain.joyeux@tidewise.io>
2022-08-15 17:51:52 -0300

    fix: disable faraday caching for github

--- lib/autoproj/daemon/git_api/services/github.rb
+++ lib/autoproj/daemon/git_api/services/github.rb
@@ -49,9 +49,6 @@ module Autoproj
                         super(**options)
 
                         stack = Faraday::RackBuilder.new do |builder|
-                            builder.use Faraday::HttpCache, serializer: Marshal,
-                                                            shared_cache: false
-
                             builder.use Octokit::Middleware::FollowRedirects
                             builder.use Octokit::Response::RaiseError
                             builder.adapter Faraday.default_adapter
